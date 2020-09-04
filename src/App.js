eb0df51da13b27ec451a1537e05999a20807d871
funmi7 <talabifunmilayo@gmail.com>
2020-09-04 10:24:26 +0100

    redux set up and signup/login form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { connect } from "react-redux";
+import { Route, Redirect, Switch } from "react-router-dom";
+import SignUpForm from "./components/forms/SignUp";
+import Login from "./components/forms/Login";
+import Issues from "./components/Issues";
 
 function App() {
+  if (localStorage.getItem("token")) {
+    return (
+      <>
+        <Switch>
+          <Route exact path="/" component={Issues} />
+          <Redirect to="/" />
+        </Switch>
+      </>
+    );
+  }
   return (
     <div className="App">
-     
+      <Switch>
+        <Route path="/signup" component={SignUpForm} />
+        <Route exact path="/login" component={Login} />
+        <Redirect to="/login" />
+      </Switch>
     </div>
   );
 }
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    isLoggedIn: state.userReducer.isLoggedIn,
+  }
+}
+export default connect(mapStateToProps)(App);
+
--- src/index.js
+++ src/index.js
@@ -4,11 +4,51 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter as Router } from "react-router-dom";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import logger from 'redux-logger';
+import { createStore, applyMiddleware, compose } from 'redux';
+import rootReducer from "./state/reducers";
 
+
+function saveToLocalStorage(state) {
+  try {
+    const serializedState = JSON.stringify(state);
+    localStorage.setItem("state", serializedState);
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+function loadFromLocalStorage() {
+  try {
+    const serializedState = localStorage.getItem("state");
+    if (serializedState === null) return undefined;
+    return JSON.parse(serializedState);
+  } catch (e) {
+    return undefined;
+  }
+}
+
+const persistedState = loadFromLocalStorage();
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+  rootReducer,
+  persistedState,
+  composeEnhancers(applyMiddleware(logger, thunk))
+);
+
+store.subscribe(() => saveToLocalStorage(store.getState()));
 ReactDOM.render(
+  <Provider store={store}>
+    <Router>
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
+  </Router>
+  </Provider>,
   document.getElementById('root')
 );
 
